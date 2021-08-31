cf37838a7649de8abdd013aa127221b1a28c54d9
Zehrei Verj Morales <moraleszehreiverj8@gmail.com>
2021-08-31 12:59:25 +0800

    Remove Comments

--- main.py
+++ main.py
@@ -8,11 +8,3 @@ if __name__ == "__main__":
     sys.exit(app.exec_())
 
 # TestTest!1
-# self.View.run_loading_screen
-# self.View.stop_loading_screen
-# self.Controller.init_register_admin
-# self.Controller.View.init_register_admin
-# self.Model.register_admin
-# self.View.txt_repeat_password.text()
-# self.Model.register_admin_qna
-# self.View.question_and_answer
